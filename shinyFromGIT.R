28152dcf9aa9c8e4d6c996ff0fca5847edf48cb6
gcarrghub <gregory.j.carr@gmail.com>
2020-06-24 10:28:39 -0400

    repository

--- shinyFromGIT.R
+++ shinyFromGIT.R
@@ -19,6 +19,7 @@ cleanUp()
 #### Details given in if(FALSE){} blocks are details details
 #### Before running the tool need to check for required packages and install if necessary
 #### Typically should only need to do this once per installation of R
+options(repos = "https://cloud.r-project.org/")
 packages = c("shiny", "shinydashboardPlus","isotone", "gridExtra", "openxlsx", "optimx", "plotrix","devtools","rstudioapi","colourpicker")
 packageTests <- sapply(packages,FUN = require,character.only=TRUE)
 if(all(packageTests)){
