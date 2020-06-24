b12c3b9be776ecbde900d5130d0772e2764e8e69
gcarrghub <gregory.j.carr@gmail.com>
2020-06-24 15:02:52 -0400

    move pkg mgmt to separate file

--- LAshinyFuns.R
+++ LAshinyFuns.R
@@ -10,56 +10,7 @@ cleanUp <- function(){
 }
 cleanUp()
 
-####
-#### In all cases, should be able to just do the following (as in, the equivalent to sourcing the file)
-#### Details given in if(FALSE){} blocks are details details
-#### Before running the tool need to check for required packages and install if necessary
-#### Typically should only need to do this once per installation of R
-#options(repos = "https://cloud.r-project.org/")
-packages = c("shiny", "shinydashboardPlus","isotone", "gridExtra", "openxlsx", "optimx", "plotrix","devtools","rstudioapi","colourpicker")
-packageTests <- sapply(packages,FUN = require,character.only=TRUE)
-print(packageTests)
-if(all(packageTests)){
-  cat("\n",paste(rep("#",100),collapse = ""),
-      "\n  All required packages are present.",
-      "\n",paste(rep("#",100),collapse = ""),"\n")
-}
-if(sum(!packageTests)>0){
-  cat("\n",paste(rep("#",100),collapse = ""),
-      "\n  Please wait while these required packages and their dependencies are installed:",
-      "\n   ",paste(names(packageTests[!packageTests]),collapse = " "),
-      "\n  Requires internet access and sufficient rights to install R packages on your system.",
-      "\n",paste(rep("#",100),collapse = ""),"\n")
-  install.packages(packages[!packageTests], repos = "https://cran.rstudio.com/", dependencies=TRUE)
-  ### In one case, needed to add this to a users install.packages call:  INSTALL_opts = c('--no-lock')
-  # recheck for packages
-  packageTests <- sapply(packages,FUN = require,character.only=TRUE)
-  print(packageTests)
-  if(all(packageTests)){
-    cat("\n",paste(rep("#",100),collapse = ""),
-        "\n  All required packages were successfully installed.",
-        "\n",paste(rep("#",100),collapse = ""),"\n")
-  }
-  if(!all(packageTests)){
-    cat("\n",paste(rep("#",100),collapse = ""),
-        "\n  Not all packages were successfully installed:",
-        "\n   ",paste(names(packageTests[!packageTests]),collapse = " "),
-        "\n",paste(rep("#",100),collapse = ""),"\n")
-  }
-}
 
-library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
-#library(shinyIncubator)
-#library(XLConnect)
-library(openxlsx)
-library(optimx)
-library(plotrix)
-library(grid)
-library(gridExtra)
-library(colourpicker)
-library(isotone)
 
 
 ### LCx functions for Shiny tool
